5e25ba504630aa0bbd676bc1dbafa4a8d5251137
Kim Taylor <kim.taylor4@live.vu.edu.au>
2015-09-09 12:14:03 +1000

    BACnet client and server working

--- src/bacnet_server.c
+++ src/bacnet_server.c
@@ -8,16 +8,10 @@
 #include <libbacnet/client.h>
 #include <libbacnet/txbuf.h>
 #include <libbacnet/tsm.h>
-#include <libbacnet/bactext.h>
 #include "bacnet_namespace.h"
 
-#define TARGET_DEVICE		    120
-#define TARGET_OBJECT_TYPE	    bacnet_OBJECT_DEVICE
-#define TARGET_OBJECT_INSTANCE	    TARGET_DEVICE
-#define TARGET_OBJECT_PROPERTY	    bacnet_PROP_OBJECT_LIST
-#define TARGET_OBJECT_INDEX	    BACNET_ARRAY_ALL
-
-#define BACNET_PORT		    0xBAC1
+#define BACNET_INSTANCE_NO	    120
+#define BACNET_PORT		    0xBAC0
 #define BACNET_INTERFACE	    "lo"
 #define BACNET_DATALINK_TYPE	    "bvlc"
 #define BACNET_SELECT_TIMEOUT_MS    1	    /* ms */
@@ -26,7 +20,7 @@
 #define BACNET_BBMD_ADDRESS	    "127.0.0.1"
 #define BACNET_BBMD_TTL		    90
 
-#define RUN_AS_BBMD_CLIENT	    1
+#define RUN_AS_BBMD_CLIENT	    0
 
 #define INC_TIMER(reset, func)	\
     do {			\
@@ -35,10 +29,6 @@
 	    func();		\
 	}			\
     } while (0)
-
-static bool error;
-static BACNET_ADDRESS target_address;
-static int request_invoke_id;
 
 static object_functions_t server_objects[] = {
     {bacnet_OBJECT_DEVICE,
@@ -84,9 +74,6 @@ static void minute_tick(void) {
 #define S_PER_MIN 60
 static void second_tick(void) {
     static int timer = S_PER_MIN;
-
-    /* Bail out after 1 second */
-    error = true;
 
     /* Invalidates stale BBMD foreign device table entries */
     bacnet_bvlc_maintenance_timer(1);
@@ -137,92 +124,19 @@ static void ms_tick(void) {
     bacnet_apdu_set_confirmed_handler(			\
 		    SERVICE_CONFIRMED_##service,	\
 		    bacnet_handler_##handler)
-#define BN_CON_ACK(service, handler) \
-    bacnet_apdu_set_confirmed_ack_handler(		\
-		    SERVICE_CONFIRMED_##service,	\
-		    handler)
-#define BN_ERR(service, handler) \
-    bacnet_apdu_set_error_handler(			\
-		    SERVICE_CONFIRMED_##service,	\
-		    handler)
-
-static void abort_handler(
-		BACNET_ADDRESS *src,
-		uint8_t invoke_id,
-		uint8_t abort_reason,
-		bool server) {
-    if (bacnet_address_match(&target_address, src) && 
-		    (invoke_id == request_invoke_id)) {
-	fprintf(stderr, "BACnet Abort: %s\n",
-	    bactext_abort_reason_name(abort_reason));
-	error = true;
-    }
-}
-
-static void reject_handler(
-		BACNET_ADDRESS *src,
-		uint8_t invoke_id,
-		uint8_t reject_reason) {
-    if (bacnet_address_match(&target_address, src) && 
-		    (invoke_id == request_invoke_id)) {
-	fprintf(stderr, "BACnet Reject: %s\n",
-	    bactext_reject_reason_name(reject_reason));
-	error = true;
-    }
-}
-
-static void read_property_err(
-		BACNET_ADDRESS *src,
-		uint8_t invoke_id,
-		BACNET_ERROR_CLASS error_class,
-		BACNET_ERROR_CODE error_code) {
-    if (bacnet_address_match(&target_address, src) && 
-		    (invoke_id == request_invoke_id)) {
-	fprintf(stderr, "BACnet Error: %s: %s\n",
-	    bactext_error_class_name(error_class),
-	    bactext_error_code_name(error_code));
-	error = true;
-    }
-}
-
-static void read_property_ack(
-		uint8_t *service_request,
-		uint16_t service_len,
-		BACNET_ADDRESS *src,
-		BACNET_CONFIRMED_SERVICE_ACK_DATA *service_data) {
-    int len;
-    BACNET_READ_PROPERTY_DATA data;
-
-    if (bacnet_address_match(&target_address, src) &&
-		    (service_data->invoke_id == request_invoke_id)) {
-	len = bacnet_rp_ack_decode_service_request(
-			service_request, service_len, &data);
-	if (len < 0) {
-	    fprintf(stderr, 
-			"Read Property ACK service request decode failed\n");
-	} else {
-	    bacnet_rp_ack_print_data(&data);
-	}
-    }
-}
 
 int main(int argc, char **argv) {
     uint8_t rx_buf[bacnet_MAX_MPDU];
     uint16_t pdu_len;
     BACNET_ADDRESS src;
-    bool found = false;
-    unsigned max_apdu;
 
-    bacnet_Device_Set_Object_Instance_Number(BACNET_MAX_INSTANCE);
+    bacnet_Device_Set_Object_Instance_Number(BACNET_INSTANCE_NO);
     bacnet_address_init();
 
     /* Setup device objects */
     bacnet_Device_Init(server_objects);
-    BN_UNC(I_AM, i_am_bind);
-    BN_CON_ACK(READ_PROPERTY, read_property_ack);
-    BN_ERR(READ_PROPERTY, read_property_err);
-    bacnet_apdu_set_abort_handler(abort_handler);
-    bacnet_apdu_set_reject_handler(reject_handler);
+    BN_UNC(WHO_IS, who_is);
+    BN_CON(READ_PROPERTY, read_property);
 
     bacnet_BIP_Debug = true;
     bacnet_bip_set_port(htons(BACNET_PORT));
@@ -232,32 +146,10 @@ int main(int argc, char **argv) {
     memset(&src, 0, sizeof(src));
 
     register_with_bbmd();
-    
-    bacnet_Send_WhoIs(TARGET_DEVICE, TARGET_DEVICE);
-		
-    while (!error) {
-	if (!found) found = bacnet_address_bind_request(
-			TARGET_DEVICE, &max_apdu, &target_address);
 
-	if (found) {
-	    if (!request_invoke_id)
-		request_invoke_id = bacnet_Send_Read_Property_Request(
-				    TARGET_DEVICE,
-				    TARGET_OBJECT_TYPE,
-				    TARGET_OBJECT_INSTANCE,
-				    TARGET_OBJECT_PROPERTY,
-				    TARGET_OBJECT_INDEX);
-	    else if (bacnet_tsm_invoke_id_free(request_invoke_id))
-		/* Transaction is finished */
-		break;
-	    else if (bacnet_tsm_invoke_id_failed(request_invoke_id)) {
-		fprintf(stderr, "Error: TSM Timeout\n");
-		bacnet_tsm_free_invoke_id(request_invoke_id);
-		error = true;
-		break;
-	    }
-	}
+    bacnet_Send_I_Am(&bacnet_Handler_Transmit_Buffer[0]);
 
+    while (1) {
 	pdu_len = bacnet_datalink_receive(
 		    &src, rx_buf, bacnet_MAX_MPDU, BACNET_SELECT_TIMEOUT_MS);
 
@@ -266,5 +158,5 @@ int main(int argc, char **argv) {
 	ms_tick();
     }
 
-    return error;
+    return 0;
 }
